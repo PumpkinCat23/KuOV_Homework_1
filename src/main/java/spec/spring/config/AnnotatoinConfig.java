8bd3d7e15e69328ffa56a95d95ee98b1c1613780
PumpkinCat23 <kuptsova@ano.msk.ru>
2022-03-31 10:55:00 +0300

    Домашнее задание
    не работает List.of

---
+++ src/main/java/spec/spring/config/AnnotatoinConfig.java
@@ -0,0 +1,50 @@
+package spec.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import spec.spring.bean.*;
+
+import java.util.List;
+
+@Configuration
+@ComponentScan("spec.spring.bean")
+public class AnnotatoinConfig {
+    //методы для RAM - List
+    @Bean
+    public List<RAM> ramList () {
+        return List.of(kingstonRam(), sonyRam(), kingstonRam());
+    }
+
+    @Bean
+    @Scope("prototype")
+    public RAM kingstonRam () {
+        return new KingstonRAM();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public RAM sonyRam () {
+        return new SonyRAM();
+    }
+
+    //методы для STORAGE - List
+    @Bean
+    public List<Storage> storageList () {
+        return List.of(dexpStorage(), hpStorage());
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Storage dexpStorage () {
+        return new DexpSTORAGE();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Storage hpStorage () {
+        return new HpSTORAGE();
+    }
+
+}
---
+++ src/main/java/spec/spring/config/JavaConfig.java
@@ -0,0 +1,69 @@
+package spec.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import spec.spring.bean.*;
+
+import java.util.List;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean("myComputer")
+    public Computer computer() {
+        Computer computer = new Computer();
+        computer.setCpu(intelCpu());
+        computer.setRamList(List.of(sonyRam(), kingstonRam(), sonyRam()));
+
+        computer.setScreen(asusScreen());
+        computer.setStorageList(List.of(dexpStorage(),hpStorage()));
+        return computer;
+    }
+
+    @Bean
+    public CPU amdCpu() {
+        return new AmdCPU();
+    }
+
+    @Bean
+    public CPU intelCpu() {
+        return new IntelCPU();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public RAM kingstonRam() {
+        return new KingstonRAM();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public RAM sonyRam () {
+        return new SonyRAM();
+    }
+
+
+    @Bean
+    public Screen asusScreen () {
+        return new AsusSCREEN();
+    }
+
+    @Bean
+    public Screen dellScreen () {
+        return new DellSCREEN();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Storage dexpStorage () {
+        return new DexpSTORAGE();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Storage hpStorage () {
+        return new HpSTORAGE();
+    }
+
+}
